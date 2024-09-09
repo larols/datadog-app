ea243be1d0b60f12c58530693d16bbaa372cc3de
larols <lars.olsson@datadoghq.com>
2024-09-09 15:40:02 +0200

    auto version

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ html_template = '''
                 (url) => url.startsWith("http://")
             ],
       env: 'production',
-      version: '1.7.0',
+      version: 'VERSION',
       sessionSampleRate: 100,
       sessionReplaySampleRate: 100,
       trackUserInteractions: true,
