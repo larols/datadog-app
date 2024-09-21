82fc50af512f6794e3ee2efaa2fa9a6a4b420985
larols <lars.olsson@datadoghq.com>
2024-09-21 23:42:07 +0200

    stuff

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -6,7 +6,12 @@ function App() {
 
     useEffect(() => {
         fetch('/api/views')
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
             .then(data => setViewsData(data))
             .catch(error => console.error('Error fetching data:', error));
     }, []);
