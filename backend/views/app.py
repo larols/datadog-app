58ac36d20271c1d1716efafa85d7e1092323e062
larols <lars.olsson@datadoghq.com>
2024-09-22 00:18:18 +0200

    logging backend

--- backend/views/app.py
+++ backend/views/app.py
@@ -9,10 +9,11 @@ patch(logging=True)  # Enable tracing for logging
 # Initialize the Flask app
 app = Flask(__name__)
 
-# Define logging format
-FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] - %(message)s')
+# Define logging format including Datadog trace information
+FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+          '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
+          '- %(message)s')
 
-# Configure logging
 logging.basicConfig(format=FORMAT)
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
