a67a37ce89c8abb48c748ac2dad961c967a6d60e
larols <lars.olsson@datadoghq.com>
2024-09-21 21:16:13 +0200

    app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Importing the App component
+import App from './app'; // Importing the App component
 import './index.css'; // Optional: global styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
