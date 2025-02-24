29e108409692816d704ce65de626ca0f6270d190
larols <lars.olsson@datadoghq.com>
2025-02-24 09:42:25 +0100

    version tagging

--- angular-frontend/src/main.ts
+++ angular-frontend/src/main.ts
@@ -3,13 +3,15 @@ import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { datadogRum } from '@datadog/browser-rum';
 
-//Initialize Datadog RUM
+const version = process.env.VERSION || 'local-dev';
+
 datadogRum.init({
     applicationId: '7cf4d64a-6b98-4af2-9470-a105d4087ddb',
     clientToken: 'pub472bdbb68604d7cf6d5339900f98f7e8',
     site: 'datadoghq.eu',
     service: 'datadog-app-angular',
     env: 'production',
+    version: version,
     sessionSampleRate: 100,
     sessionReplaySampleRate: 100,
     trackUserInteractions: true,
@@ -18,7 +20,6 @@ datadogRum.init({
     defaultPrivacyLevel: 'allow'
 });
 
-//Capture all global errors
 window.addEventListener('error', (event) => {
   datadogRum.addError(event.error || event.message, { source: 'window' });
 });
@@ -26,10 +27,11 @@ window.addEventListener('error', (event) => {
 window.addEventListener('unhandledrejection', (event) => {
   datadogRum.addError(event.reason, { source: 'promise' });
 });
-
-//Start Angular
+r
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => {
     datadogRum.addError(err, { source: 'bootstrap' });
     console.error(err);
   });
+
+console.log(`🚀 Datadog RUM initialized with version: ${version}`);
