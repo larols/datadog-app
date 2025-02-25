b9fb0470f45d3201e4f343d270aef2475c17497c
larols <lars.olsson@datadoghq.com>
2025-02-25 23:24:58 +0100

    tests

--- frontend/src/app.test.js
+++ frontend/src/app.test.js
@@ -2,14 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './app';
 
-test('renders app title', () => {
+test('renders the app title', () => {
     render(<App />);
-    const titleElement = screen.getByText(/datadog-app/i);
-    expect(titleElement).toBeInTheDocument();
-});
-
-test('renders "Reload UID Data" button', () => {
-    render(<App />);
-    const buttonElement = screen.getByText(/Reload UID Data/i);
-    expect(buttonElement).toBeInTheDocument();
+    expect(screen.getByText(/datadog-app/i)).toBeInTheDocument();
 });
